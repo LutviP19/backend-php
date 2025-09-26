846e7d9451f5aa3e139dd58bbd5e847a92d7da9d
LutviP19 <lutvip19@gmail.com>
2025-09-27 00:03:04 +0700

    websocket server

--- servers/api-server.php
+++ servers/api-server.php
@@ -17,9 +17,19 @@ use OpenSwoole\Http\Response as OpenSwooleResponse;
 
 $server = new Server($serverip, $serverport);
 $server->set([
+    // Worker
+    'worker_num' => 4,      // The number of worker processes to start
+    'task_worker_num' => 4,  // The amount of task workers to start
+    'backlog' => 128,       // TCP backlog connection number
+
     // Disable coroutines to safely access $_SESSION
     'enable_coroutine' => false,
 ]);
+
+// Triggered when new worker processes starts
+$server->on("WorkerStart", function ($server, $workerId) {
+    // ...
+});
 
 // Start Server
 $server->on("Start", function (Server $server) {
@@ -28,22 +38,22 @@ $server->on("Start", function (Server $server) {
     echo "Swoole http server is started at http://" . $serverip . ":" . $serverport . "\n";
 });
 
-// $server->on("Connect", function (Server $server, int $fd) {
-//     global $sessID;
+$server->on("Connect", function (Server $server, int $fd) {
+    global $sessID;
 
-//     $clientInfo = $server->getClientInfo($fd);
-//     // $sessID = custom_session_regenerate_id();
+    // $clientInfo = $server->getClientInfo($fd);
+    // // $sessID = custom_session_regenerate_id();
 
-//     if(session_status() == PHP_SESSION_ACTIVE)
-//         session_destroy();
+    // if(session_status() == PHP_SESSION_ACTIVE)
+    //     session_destroy();
 
-//     if ($clientInfo) {
-//         echo "Client connected: " . $clientInfo['remote_ip'] . "\n";
-//         echo "Client FD: " . $fd . "\n";
-//         echo "Http sessStatus: " . session_status() . "\n";
-//         echo "Http sessID: " . $sessID . "\n";
-//     }
-// });
+    if ($clientInfo) {
+        echo "Client connected: " . $clientInfo['remote_ip'] . "\n";
+        echo "Client FD: " . $fd . "\n";
+        echo "Http sessStatus: " . session_status() . "\n";
+        echo "Http sessID: " . $sessID . "\n";
+    }
+});
 
 $server->on('request', new SessionDecorator(function (OpenSwooleRequest $request, OpenSwooleResponse $response) {
     global $sessID;
@@ -73,7 +83,7 @@ $server->on('request', new SessionDecorator(function (OpenSwooleRequest $request
 
     // Get header metadata
     $headers = getallheaders();
-    
+
     $userAgent = $request->header['user-agent'];
     echo "Client sessID: " . session_id() . "\n";
     echo  "Client User-Agent: " . $userAgent . PHP_EOL . "---------" . PHP_EOL;
@@ -87,7 +97,26 @@ $server->on('request', new SessionDecorator(function (OpenSwooleRequest $request
     $response->write(json_encode($_SESSION));
     $response->end();
 
+    // // Handle API logic based on $request->server['request_uri'] and $request->getMethod()
+    // if ($request->server['request_uri'] === '/api/users' && $request->getMethod() === 'GET') {
+    //     $response->header("Content-Type", "application/json");
+    //     $response->end(json_encode(['users' => [['id' => 1, 'name' => 'Alice'], ['id' => 2, 'name' => 'Bob']]]));
+    // } else {
+    //     $response->status(404);
+    //     $response->end("Not Found");
+    // }
+
     // session_write_close();
 }, 'session_create_id', true));
+
+// Triggered when the server is shutting down
+$server->on("Shutdown", function ($server, $workerId) {
+    // ...
+});
+
+// Triggered when worker processes are being stopped
+$server->on("WorkerStop", function ($server, $workerId) {
+    // ...
+});
 
 $server->start();
---
+++ servers/websocket-server.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+// // Disabled Log Errors
+// ini_set('log_errors', 0);
+// // ini_set('display_errors', 0);
+// // ini_set('display_startup_errors', 0);
+error_reporting(~E_NOTICE & ~E_DEPRECATED & ~E_WARNING);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+use OpenSwoole\WebSocket\Server;
+use OpenSwoole\Http\Request;
+use OpenSwoole\WebSocket\Frame;
+
+$server = new Server("0.0.0.0", 9502);
+
+$server->on("Start", function (Server $server) {
+    echo "OpenSwoole WebSocket Server is started at http://127.0.0.1:9502\n";
+});
+
+$server->on('Open', function (Server $server, OpenSwoole\Http\Request $request) {
+    echo "connection open: {$request->fd}\n";
+
+    $server->tick(1000, function () use ($server, $request) {
+        $server->push($request->fd, json_encode(["hello", time()]));
+    });
+});
+
+$server->on('Message', function (Server $server, Frame $frame) {
+    echo "received message: {$frame->data}\n";
+    $server->push($frame->fd, json_encode(["hello", time()]));
+});
+
+$server->on('Close', function (Server $server, int $fd) {
+    echo "connection close: {$fd}\n";
+});
+
+$server->on('Disconnect', function (Server $server, int $fd) {
+    echo "connection disconnect: {$fd}\n";
+});
+
+$server->start();

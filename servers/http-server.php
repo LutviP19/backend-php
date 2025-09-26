8369cb2f1e71518a6a394918a3ae371532576e1b
LutviP19 <lutvip19@gmail.com>
2025-09-26 15:08:22 +0700

    php server

--- servers/http-server.php
+++ servers/http-server.php
@@ -496,61 +496,6 @@ function createTmp($fd, $fileName, $setHeaders, $content)
     return $filePath;
 }
 
-function initializeServerConstant($request) //OpenSwoole\Http\Request
-{// Setup
-    global $serverip, $serverport;
-
-    $_SERVER = [];
-    $uri = $request->server["request_uri"] ?? "/";
-    $requestip = $request->server["remote_addr"] ?? clientIP();
-
-    $_SERVER['DOCUMENT_ROOT'] = __DIR__;
-    $_SERVER['SERVER_NAME'] = $serverip;
-    $_SERVER['SERVER_PORT'] = $serverport;
-    $_SERVER['SERVER_SOFTWARE'] = "Backend PHP";
-    $_SERVER['SERVER_PROTOCOL'] = isset($request->server['server_protocol']) ? $request->server['server_protocol'] : null;
-    $_SERVER['HTTP_HOST'] = $serverip.":".$serverport;
-    $_SERVER['HTTP_ACCEPT'] = isset($request->header['accept']) ? $request->header['accept'] : "*";
-    $_SERVER['HTTP_USER_AGENT'] = $request->header['user-agent'] ?? null;
-    $_SERVER['HTTP_ACCEPT_ENCODING'] = $request->header['accept-encoding'] ?? null;
-    $_SERVER['QUERY_STRING'] = isset($request->server['query_string']) ? $request->server['query_string'] : null;
-    $_SERVER['PHP_SELF'] = isset($request->server['php_self']) ? $request->server['php_self'] : null;
-    $_SERVER['SCRIPT_NAME'] = isset($request->server['script_name']) ? $request->server['script_name'] : null;
-    $_SERVER['SCRIPT_FILENAME'] = isset($request->server['script_filename']) ? $request->server['script_filename'] : null;
-    $_SERVER['REMOTE_ADDR'] = $requestip;
-    $_SERVER['REMOTE_PORT'] = isset($request->server['remote_port']) ? $request->server['remote_port'] : null;
-    $_SERVER['REQUEST_URI'] = $uri;
-    $_SERVER['REQUEST_METHOD'] = $request->server["request_method"];
-    $_SERVER['REQUEST_TIME'] = isset($request->server['request_time']) ? $request->server['request_time'] : null;
-    $_SERVER['REQUEST_TIME_FLOAT'] = isset($request->server['request_time_float']) ? $request->server['request_time_float'] : null;
-
-    foreach ($request->server as $key => $value) {
-        $_SERVER[strtoupper($key)] = $value;
-    }
-
-    $_REQUEST = [];
-    $_GET = $request->get ?? [];
-    $_POST = $request->post ?? [];
-    $_FILES = $request->files ?? [];
-    $_COOKIE = $request->cookie ?? [];
-
-    $_REQUEST = array_merge($_GET, $_POST);
-
-    foreach ($request->header as $key => $value) {
-        $_SERVER['HTTP_' . strtoupper(str_replace('-', '_', $key))] = $value;
-    }
-
-    if (isset($request->header['host'])) {
-        $_SERVER['HTTP_HOST'] = $request->header['host'];
-    }
-
-    if (isset($request->cookie)) {
-        foreach ($request->cookie as $key => $value) {
-            $_COOKIE[$key] = $value;
-        }
-    }
-}
-
 function startSession($response)
 {
     global $redis, $sessID;

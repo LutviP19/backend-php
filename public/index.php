e61ebf97f4797f1e99e87ea5479ffd66e23b0619
LutviP19 <lutvip19@gmail.com>
2025-12-01 08:08:51 +0700

    Blocked boot agent

--- public/index.php
+++ public/index.php
@@ -1,5 +1,23 @@
 <?php
 
+// Blocked boot agent
+$blocked_agents = [
+    'python-httpx',
+    'go-http-client',
+];
+
+$user_agent = trim($_SERVER['HTTP_USER_AGENT'] ?? '');
+// die($user_agent);
+
+foreach ($blocked_agents as $agent) { 
+    if (str_contains(strtolower($user_agent), strtolower($agent))) {
+        header('HTTP/1.0 403 Forbidden');
+        exit('Access denied.');
+    }
+}
+// END Blocked boot agent
+
+
 if (!defined('APP_START')) {
     define('APP_START', microtime(true));
 }

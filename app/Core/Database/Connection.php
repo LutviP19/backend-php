733524e2b74743c2df3eaf6f768581ee18136e05
LutviP19 <lutvip19@gmail.com>
2026-06-27 16:17:45 +0700

    fix Auth DB

--- app/Core/Database/Connection.php
+++ app/Core/Database/Connection.php
@@ -1,56 +1,56 @@
 <?php
 
+/**
+ * Connection class
+ * @package Backend-PHP
+ * @author LutviP19 <lutvip19@gmail.com>
+ */
+
 namespace App\Core\Database;
 
 use PDO;
 use PDOException;
 use App\Core\Support\Config;
 
-/**
- * Connection Class
- *  @author Lutvi <lutvip19@gmail.com>
- */
 class Connection
-{    
+{
     /**
      * Method make PDO connection
      *
      * @return void
      */
-    public static function make()
+    public static function make(): PDO
     {
         try {
-            $driver = Config::get('default_db');
+            $driver = Config::get("default_db");
 
-
-            if ($driver !== 'sqlite') {
-                $name     = Config::get("database.{$driver}.dbname");
-                $host     = Config::get("database.{$driver}.host");
-                $port     = Config::get("database.{$driver}.port");
+            if ($driver !== "sqlite") {
+                $dbname = Config::get("database.{$driver}.dbname");
+                $host = Config::get("database.{$driver}.host");
+                $port = Config::get("database.{$driver}.port");
                 $username = Config::get("database.{$driver}.username");
                 $password = Config::get("database.{$driver}.password");
-                $options  = Config::get("database.{$driver}.options");
-                // dd("{$driver}:host={$host};port={$port};dbname={$name}");
+                $options = Config::get("database.{$driver}.options");
 
-                $pdo = new PDO(
-                    "{$driver}:host={$host};port={$port};dbname={$name}",
-                    $username,
-                    $password,
-                    $options
-                );
+                $dsn = "{$driver}:host={$host};port={$port};dbname={$dbname}";
+                if ($driver === "mysql") {
+                    $dsn = "{$driver}:host={$host};port={$port};dbname={$dbname};charset=utf8mb4";
+                }
 
+                // dd($dsn);
+                $pdo = new PDO($dsn, $username, $password, $options);
             } else {
                 $databaseFile = Config::get("database.{$driver}.dbname");
-                // dd("sqlite:{$databaseFile}");
-
                 $pdo = new PDO("sqlite:{$databaseFile}");
-                // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode for better error handling
+
+                // Set error mode for better error handling
+                if (config("app.debug")) {
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                }
             }
             // dd($pdo->getAttribute(PDO::ATTR_DRIVER_NAME));
 
-
             return $pdo;
-
         } catch (PDOException $e) {
             die($e->getMessage());
         }
@@ -61,38 +61,52 @@ class Connection
      *
      * @return void
      */
-    public static function custom($driver = '', $name = '', $host = '', $port = '', $username = '', $password = '', $options = [])
-    {
+    public static function custom(
+        string $driver = "",
+        string $dbname = "",
+        string $host = "",
+        string $port = "",
+        string $username = "",
+        string $password = "",
+        ?array $options = [],
+    ): PDO {
         try {
-            $driver = $driver ?: Config::get('default_db');
+            $driver = $driver ?: Config::get("default_db");
+            $options = array_merge($options, Config::get("database.{$driver}.options"));
+            // dd($options);
 
+            if ($driver !== "sqlite") {
+                $dsn = "{$driver}:host={$host};port={$port};dbname={$dbname}";
+                if ($driver === "mysql") {
+                    $dsn = "{$driver}:host={$host};port={$port};dbname={$dbname};charset=utf8mb4";
+                }
 
-            if ($driver !== 'sqlite') {
-                // dd("{$driver}:host={$host};port={$port};dbname={$name}");
-                // dd(['username' => $username, 'password' => $password, 'options' => $options]);
-
-                $pdo = new PDO(
-                    "{$driver}:host={$host};port={$port};dbname={$name}",
-                    $username,
-                    $password,
-                    $options
-                );
-
+                // dd($dsn);
+                $pdo = new PDO($dsn, $username, $password, $options);
             } else {
-                $databaseFile = database_path($name);
-                // dd("sqlite:{$databaseFile}");
-
+                $databaseFile = database_path($dbname);
                 $pdo = new PDO("sqlite:{$databaseFile}");
-                // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode for better error handling
+
+                // Set error mode for better error handling
+                if (config("app.debug")) {
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                }
             }
             // dd($pdo->getAttribute(PDO::ATTR_DRIVER_NAME));
 
-
             return $pdo;
-
         } catch (PDOException $e) {
-            die($e->getMessage());
+            // die($e->getMessage());
+            if (config("app.env") === "production") {
+                if (config("app.debug")) {
+                    \write_log($e->getMessage(), \App\Core\Database\Connection::class, "error", "error_DB.log");
+                }
+
+                json_response([], 403, "Auth errors", ["auth" => "Invalid credentials."]);
+            } else {
+                json_response([], 403, "Auth errors", ["auth" => $e->getMessage()]);
+            }
+            die();
         }
     }
-
 }
--- storage/database/migrations/mysql/users.sql
+++ storage/database/migrations/mysql/users.sql
@@ -25,4 +25,4 @@ CREATE TABLE `users` (
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
